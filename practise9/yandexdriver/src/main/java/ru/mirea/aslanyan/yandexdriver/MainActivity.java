e666cb5e7c0d181d0d062b25c42efcda108118a5
shiru38 <aslanyan88888@icloud.com>
2022-05-24 23:36:50 +0300

    asd

---
+++ practise9/yandexdriver/src/main/java/ru/mirea/aslanyan/yandexdriver/MainActivity.java
@@ -0,0 +1,110 @@
+package ru.mirea.aslanyan.yandexdriver;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.RequestPoint;
+import com.yandex.mapkit.RequestPointType;
+import com.yandex.mapkit.directions.DirectionsFactory;
+import com.yandex.mapkit.directions.driving.DrivingOptions;
+import com.yandex.mapkit.directions.driving.DrivingRoute;
+import com.yandex.mapkit.directions.driving.DrivingRouter;
+import com.yandex.mapkit.directions.driving.DrivingSession;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.runtime.Error;
+import com.yandex.runtime.network.NetworkError;
+import com.yandex.runtime.network.RemoteError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements DrivingSession.DrivingRouteListener {
+    private final String MAPKIT_API_KEY = "06be0acd-ca37-4276-bc62-65e8bececbc6";
+    private final Point ROUTE_START_LOCATION = new Point(55.670005, 37.479894);
+    private final Point ROUTE_END_LOCATION = new Point(55.794229, 37.700772);
+    private final Point SCREEN_CENTER = new Point(
+            (ROUTE_START_LOCATION.getLatitude() + ROUTE_END_LOCATION.getLatitude()) / 2,
+            (ROUTE_START_LOCATION.getLongitude() + ROUTE_END_LOCATION.getLongitude()) / 2);
+
+    private MapView mapView;
+    private DrivingSession drivingSession;
+    private MapObjectCollection mapObjects;
+    private DrivingRouter router;
+    private int[] colors = {0xFFFF0000, 0xFF00FF00, 0x00FFBBBB, 0xFF0000FF};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        MapKitFactory.initialize(this);
+        DirectionsFactory.initialize(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapView = findViewById(R.id.mapview);
+        mapView.getMap().move(
+                new CameraPosition(SCREEN_CENTER, 10.0f, 0.0f, 0.0f));
+        router = DirectionsFactory.getInstance().createDrivingRouter();
+        mapObjects = mapView.getMap().getMapObjects().addCollection();
+
+        submitRequest();
+    }
+
+    @Override
+    public void onDrivingRoutes(@NonNull List<DrivingRoute> list) {
+        int color;
+        for (int i = 0; i < list.size(); i++) {
+            color = colors[i];
+            mapObjects.addPolyline(list.get(i).getGeometry()).setStrokeColor(color);
+        }
+    }
+
+    @Override
+    public void onDrivingRoutesError(@NonNull Error error) {
+        String errorMessage = getString(R.string.unknown_error_message);
+        if (error instanceof RemoteError) {
+            errorMessage = getString(R.string.remote_error_message);
+        } else if (error instanceof NetworkError) {
+            errorMessage = getString(R.string.network_error_message);
+        }
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    private void submitRequest() {
+        DrivingOptions options = new DrivingOptions();
+        // Кол-во альтернативных путей
+        options.setAlternativeCount(3);
+        ArrayList<RequestPoint> requestPoints = new ArrayList<>();
+        // Устанавливаем точки маршрута
+        requestPoints.add(new RequestPoint(
+                ROUTE_START_LOCATION,
+                RequestPointType.WAYPOINT,
+                null));
+        requestPoints.add(new RequestPoint(
+                ROUTE_END_LOCATION,
+                RequestPointType.WAYPOINT,
+                null));
+        // Делаем запрос к серверу
+        drivingSession = router.requestRoutes(requestPoints, options, this);
+    }
+
+    @Override
+    protected void onStop() {
+        MapKitFactory.getInstance().onStop();
+        mapView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+}
